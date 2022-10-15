f3b8bcf9327641b3396143393fa2add42f2826a7
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2022-10-15 19:57:12 +0200

    Disable extention outside of udemy/course page

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.tabs.onActivated.addListener(async info => {
     const tab = await chrome.tabs.get(info.tabId);
-    
-    const isGithub = tab.url.startsWith('https://github.com/');
-    isGithub 
-      ? chrome.action.enable(tab.tabId) 
-      : chrome.action.disable(tab.tabId);
+
+    const isUdemy = tab.url.includes('udemy.com/course');
+    if (isUdemy) {
+      chrome.action.setIcon({path: 'icon.png'});
+      chrome.action.enable(tab.tabId);
+    } else {
+      chrome.action.setIcon({path: 'icon_disabled.png'});
+      chrome.action.disable(tab.tabId);
+    };
   });
 });
