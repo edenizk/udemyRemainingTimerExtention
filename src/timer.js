058f54dc3a928298ba3bd3ea56b293be2a73f6a0
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2024-03-06 23:35:48 +0100

    Add current section tab

--- src/timer.js
+++ src/timer.js
@@ -1,11 +1,37 @@
-function getLeftTime() {  
-  const sections = document.querySelectorAll('[data-purpose=curriculum-section-container] > [data-purpose]');
+let panelContainer;
+let currentSection = [];
+let sections = [];
 
-  if (!sections.length) {
+async function fetchPanelContainer() {
+  const panelContainer = document.querySelector('[data-purpose="curriculum-section-container"]')
+  return panelContainer.outerHTML;
+}
+
+function fetchSections(panel) {
+  sections = panel.querySelectorAll('[data-purpose=curriculum-section-container] > [data-purpose]');
+  const currentLecture = panel.querySelector('[class*="curriculum-item-link--is-current--"]')
+  let tempCurrentSection = [];
+  if (currentLecture) {
+    currentSection = [currentLecture.closest('[data-purpose]')];
+  }
+}
+
+function getLeftTime(isFullCourse = true) {  
+  let tempSections = [];
+  if (isFullCourse) {
+    tempSections = sections;
+  } else {
+    tempSections = currentSection;
+  }
+  console.log('tempSections', tempSections)
+
+  if (!tempSections.length) {
     return {error: true, text: 'No section found.\nBe sure that you are at the udemy course tab and you started the course'};
   }
   
-  sections.forEach((section) => {
+  let totalMinutes = 0;
+
+  tempSections.forEach((section) => {
     const header = section.querySelector('div');
     const checkbox = section.querySelector('span');
     const isExpanded = checkbox.getAttribute('data-checked');
@@ -14,9 +40,14 @@ function getLeftTime() {
       header.click();
     }
   })
-  
-  let totalMinutes = 0;
-  const items = document.querySelectorAll('[data-purpose^="curriculum-item-"][class*="item-link--common--"]') // each item
+  let items = [];
+
+  if (isFullCourse) {
+    items = panelContainer.querySelectorAll('[data-purpose^="curriculum-item-"][class*="item-link--common--"]') // each item
+  } else {
+    items = tempSections[0].querySelectorAll('[data-purpose^="curriculum-item-"][class*="item-link--common--"]') // each item
+  }
+
   if (!items.length) {
     return {error: true, text: 'No items found, be sure that sections are opened'};
   }
@@ -49,27 +80,66 @@ const setTimer = (val, timerEl) => {
     timerEl.innerHTML = "Couldn't calculate try to open extension again";
     return;
   }
-  const result = val[0].result;
 
-  if (result.error) {
+  if (val.error) {
     timerEl.classList.add('error');
-    timerEl.innerHTML = result.text;
+    timerEl.innerHTML = val ? val.text : 'something went wrong 😿';
     return;
   }
 
   timerEl.classList.remove('error');
-  timerEl.innerHTML = `${result.hours}:${result.minutes} Left`;
+  timerEl.innerHTML = `${val.hours}:${val.minutes}H Left`;
 }
 
 const callback = (tabs) => {
   const currentTab = tabs[0]; // there will be only one in this array
+  const wholeCourseButton = document.querySelector('.whole-course')
+  const currentSectionButton = document.querySelector('.current-section')
   const timerEl = document.querySelector('#timer');
   timerEl.innerHTML = 'Calculating...';
 
+  wholeCourseButton.addEventListener('click', () => {
+    console.log('button clicked 1')
+    if (wholeCourseButton.classList.contains('active')) return;
+
+    wholeCourseButton.classList.add('active');
+    currentSectionButton.classList.remove('active');
+    const val = getLeftTime();
+    console.log('val', val)
+    console.log('sections', sections)
+    setTimer(val, timerEl)
+  })
+
+  currentSectionButton.addEventListener('click', () => {
+    console.log('button clicked 2')
+
+    if (currentSectionButton.classList.contains('active')) return;
+
+    currentSectionButton.classList.add('active');
+    wholeCourseButton.classList.remove('active');
+    const val = getLeftTime(false);
+    console.log('sections', currentSection)
+    console.log('val', val)
+    setTimer(val, timerEl)
+  })
+
   chrome.scripting.executeScript({
     target: {tabId: currentTab.id },
-    func: getLeftTime
-  }, (val) => setTimer(val, timerEl))
+    func: fetchPanelContainer
+  }, (result) => {
+    const panel = result[0].result;
+    console.log('panel', panel)
+
+    const parser = new DOMParser();
+    panelContainer = parser.parseFromString(panel, "text/html");
+
+    fetchSections(panelContainer)
+    // currentSection = val[0].currentSection;
+    const time = getLeftTime();
+    console.log('time', time);
+    // console.log('val', val)
+    setTimer(time, timerEl)
+  })
 }
 
 var query = { active: true, currentWindow: true };
